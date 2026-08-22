f98f6aa29c0f5b1b2090e0ed6a1220724549c2dc
Lucky Sah <lk4179869@gmail.com>
2026-08-22 13:34:42 +0530

    add: activity 7

--- 2.cpp
+++ 2.cpp
@@ -62,20 +62,68 @@ int main(){
     // }
 
     //activity5
-    int num1, num2, num3;
-    cout<<"enter first number"<<endl;
-    cin>>num1;
-    cout<<"enter second number"<<endl;
-    cin>>num2;
-    cout<<"enter third number"<<endl;
-    cin>>num3;
-    if(num1+num2>num3 && num1+num3>num2 && num2+num3>num1){
-        cout<<"valid triangle"<<endl;
+    // int num1, num2, num3;
+    // cout<<"enter first number"<<endl;
+    // cin>>num1;
+    // cout<<"enter second number"<<endl;
+    // cin>>num2;
+    // cout<<"enter third number"<<endl;
+    // cin>>num3;
+    // if(num1+num2>num3 && num1+num3>num2 && num2+num3>num1){
+    //     cout<<"valid triangle"<<endl;
+    // }
+    // else{
+    //     cout<<"invalid triangle"<<endl;
+    // }
+
+    //activity 6
+    // int marks;
+    // cout<<"enter marks"<<endl;
+    // cin>>marks;
+    // if(marks>=90 && marks<=100){
+    //     cout<<"A"<<endl;
+    // }
+    // else if(marks>=75 && marks<90){
+    //     cout<<"B"<<endl;
+    // }
+    // else if(marks>=60 && marks<75){
+    //     cout<<"C"<<endl;
+    // }
+    // else if(marks>=40 && marks<60){
+    //     cout<<"D"<<endl;
+    // }
+    // else if(marks>=0 && marks<40){
+    //     cout<<"fail"<<endl;
+    // }
+    // else{
+    //     cout<<"invalid marks"<<endl;
+    // }
+
+    //activity 7
+    int unit ;
+    cout<<"enter units"<<endl;
+    cin>>unit;
+    if(unit>=0 && unit<=100){
+        cout<<unit*5<<"rs."<<endl;
+    }
+    else if(unit>100 && unit<=200){
+        int startUnit= 100*5;
+        int balUnit= unit-100;
+        int calUnit= balUnit*7;
+        cout<<startUnit+calUnit<<"rs."<<endl;
+
+    }
+    else if(unit > 200){
+        int startUnit= 100*5;
+        int secondUnit= 100*7;
+        int balUnit= unit-200;
+        int calUnit= balUnit*10;
+        cout<<startUnit+secondUnit+calUnit<<"rs."<<endl;
+
     }
     else{
-        cout<<"invalid triangle"<<endl;
+        cout<<"invalid"<<endl;
     }
-
 
     return 0;
 }
