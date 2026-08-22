1597169261872930c6ad4ad8ddc33760caab6bc1
Lucky Sah <lk4179869@gmail.com>
2026-08-22 11:37:34 +0530

    add: activity 2

--- 2.cpp
+++ 2.cpp
@@ -2,15 +2,29 @@
 using namespace std;
 int main(){
 
-    int num= 32;
-    if(num>0){
-        cout<<"positive"<<endl;
+    // int num= 32;
+    // if(num>0){
+    //     cout<<"positive"<<endl;
+    // }
+    // else if(num<0){
+    //     cout<<"negative"<<endl;
+    // }
+    // else{
+    //     cout<<"zero"<<endl;
+    // }
+
+    //activity 2
+    int num1= 1;
+    int num2= 5;
+    int num3= 3;
+    if(num1>num2 && num1>num3){
+        cout<<num1<<endl;
     }
-    else if(num<0){
-        cout<<"negative"<<endl;
+    else if(num2>num1 && num2>num3){
+        cout<<num2<<endl;
     }
     else{
-        cout<<"zero"<<endl;
+        cout<<num3<<endl;
     }
 
 
