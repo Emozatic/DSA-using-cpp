3c39575104fd85dd046dd9de21088f90ee2cbcf2
Lucky Sah <lk4179869@gmail.com>
2026-08-22 11:25:51 +0530

    operators

--- 1.cpp
+++ 1.cpp
@@ -4,9 +4,17 @@ int main(){
 
     cout<<"Hello, World"<<endl;
     int age= 21;
-    int num1 =12;
+    int num1 =21;
     int num2= 21;
+    //operators
     cout<<num1+num2<<endl;
+    cout<<num1-num2<<endl;
+    cout<<num1*num2<<endl;
+    cout<<num1/num2<<endl;
+    cout<<num1%num2<<endl;
+
+    
+
 
 
     return 0;
