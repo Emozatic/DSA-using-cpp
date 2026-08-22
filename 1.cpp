83ba6a69b20cc0a33902a0e8ec245741629091df
Lucky Sah <lk4179869@gmail.com>
2026-08-22 11:22:59 +0530

    initial commit

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"Hello, World"<<endl;
+    int age= 21;
+    int num1 =12;
+    int num2= 21;
+    cout<<num1+num2<<endl;
+
+
+    return 0;
+}
