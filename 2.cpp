a9a143684f4721c3b6c9c7ca8e6d18b9abd20a53
Lucky Sah <lk4179869@gmail.com>
2026-08-22 11:35:27 +0530

    add: conditional statements

---
+++ 2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int num= 32;
+    if(num>0){
+        cout<<"positive"<<endl;
+    }
+    else if(num<0){
+        cout<<"negative"<<endl;
+    }
+    else{
+        cout<<"zero"<<endl;
+    }
+
+
+
+    return 0;
+}
