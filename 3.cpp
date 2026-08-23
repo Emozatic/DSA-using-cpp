755322f27008402ebf5393a4f97887e6ece2e1f3
Lucky Sah <lk4179869@gmail.com>
2026-08-23 12:04:54 +0530

    add: activity 5

--- 3.cpp
+++ 3.cpp
@@ -37,13 +37,24 @@ int main(){
     // cout<<fact<<endl;
 
     //activity 4
-    int num;
-    cout<<"enter num"<<endl;
-    cin>>num;
+    // int num;
+    // cout<<"enter num"<<endl;
+    // cin>>num;
 
-    for(int i=1; i<=10; i++){
-        cout<<num<<"*"<<i<<"="<<num*i<<endl;
+    // for(int i=1; i<=10; i++){
+    //     cout<<num<<"*"<<i<<"="<<num*i<<endl;
+    // }
+
+    //activity 5
+    int num= 344, count=0;
+    while(num>0){
+        int div= num/10;
+        if(div>=0){
+            count++;
+        }
+        num= div;
     }
+    cout<<count;
 
     return 0;
 }
