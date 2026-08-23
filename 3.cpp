ce3b0e90679ab2e7f2e0e16a8fcc163d235cec4e
Lucky Sah <lk4179869@gmail.com>
2026-08-23 11:49:44 +0530

    add: activity 1

---
+++ 3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //activity 1
+    int num;
+    cout<<"enter a number"<<endl;
+    cin>>num;
+    for(int i=1; i<=num; i++){
+        cout<<i<<endl;
+    }
+
+
+    return 0;
+}
