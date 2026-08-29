3147fb70f90b339e9b244f8ca5854365ed779970
Lucky Sah <lk4179869@gmail.com>
2026-08-29 22:34:17 +0530

    add: printing even number 1 to 20 using loops

--- 4.cpp
+++ 4.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 int main(){
 
-    for (int i=1; i<=10; i++ ){
-        cout<<i<<endl;
+    // for (int i=1; i<=10; i++ ){
+
+    //     cout<<i<<endl;
+    // }
+
+    //activity 2
+    for(int i=0; i<=20; i++){
+        if(i%2==0){
+            cout<<i<<endl;
+        }
     }
 
 
